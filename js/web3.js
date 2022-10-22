4049ed0550c02ca89f48821d1f73d3fe266defee
daemondao <bitdaemons@gmail.com>
2022-10-22 20:34:47 +0100

    trewq

--- js/web3.js
+++ js/web3.js
@@ -336,6 +336,10 @@ async function fetchAccountData() {
   //populate NFTs
   await populateNFTs(selectedAccount);
   await setNumbers() //set minted/supply
+
+  placeholder
+
+
   //displayTokenName();
   //collapsible divs
   var coll = document.getElementsByClassName("collapsible");
@@ -362,6 +366,15 @@ async function fetchAccountData() {
  * - User switches networks in wallet
  * - User connects wallet initially
  */
+async function getTreasury() {
+
+  const contractBalance = await provider.getBalance("0xf9e393CbD7e8F34FB87127195f1F74E699D3d595");
+  console.log('BBC contract balance: ' + contractBalance.toString();
+  const multisigBalance = await provider.getBalance("0x5FC85515d6613164457724035413c5f8b2d899E5");
+  console.log('MultiSig balance: ' + multisigBalance.toString();
+  const flipBalance = await provider.getBalance("0x948E422Da3Bd457289C526C8bE6319949411BD5A");
+}console.log('Flipporrr balance: ' + flipBalance.toString();
+
 
 async function refreshAccountData() {
 
