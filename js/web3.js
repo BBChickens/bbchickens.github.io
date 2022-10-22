227038b63579713a425a46f6578b53cb3c0db377
daemondao <bitdaemons@gmail.com>
2022-10-22 04:13:47 +0100

    setnums

--- js/web3.js
+++ js/web3.js
@@ -647,7 +647,8 @@ async function setNumbers() {
   let contractAddress = 0xf9e393CbD7e8F34FB87127195f1F74E699D3d595;
   let theCount = await queryMinted(contractAddress);
   let theTotal = await queryAlloted(contractAddress);
-  theString = `${queryMinted}/${queryAlloted}`
+  theString = `${queryMinted}/${queryAlloted}`;
+  console.log(theString);
   document.getElementById("count").innerHTML = theString;
 }
 
