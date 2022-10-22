5518924674c47e65780883fb3909baff02f4645a
daemondao <bitdaemons@gmail.com>
2022-10-22 12:42:02 +0100

    reeeeow

--- js/web3.js
+++ js/web3.js
@@ -711,7 +711,7 @@ async function mintBBC() {
   // define tokenContract because why twice?
   let tokenContract = await new web3.eth.Contract(ABI, contractAddress);
   let mintAmount = $('#Nmint').val();
-  let ftm = number * 45000000000000000000;
+  //let ftm = number * 45000000000000000000;
   //let value = await tokenContract.methods.mint([ftm.toString(), number.toString()]).send({ from: selectedAccount });
   // let value = await tokenContract.methods.mint([ftm.toString(), number.toString()]).send({ from: selectedAccount });
   //try AMGOTH way
