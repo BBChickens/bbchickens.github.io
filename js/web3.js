da185fccec15513a90521f54a7004892d7ca06b7
daemondao <bitdaemons@gmail.com>
2022-10-23 13:30:56 +0100

    comeoooooooon

--- js/web3.js
+++ js/web3.js
@@ -354,8 +354,8 @@ async function fetchAccountData() {
   console.log('xBOO balance: ' + xBooBalance.toString() + '$xBOO'); //get treasury balances
 
   let totalFTM = contractBalance + multisigBalance + flipBalance;
-  document.getElementById("ftmtreasurybalance").innerHTML = totalFTM; //populate FTM treasury balance
-  document.getElementById("xbootreasurybalance").innerHTML = xBooBalance; //populate FTM treasury balance
+  document.getElementById("ftmTB").innerHTML = totalFTM; //populate FTM treasury balance
+  document.getElementById("xbooTB").innerHTML = xBooBalance; //populate FTM treasury balance
 
   //displayTokenName();
   //collapsible divs
