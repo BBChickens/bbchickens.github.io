ee2edc4f3e98c6717d30233a3bef04d1b0bb77e8
daemondao <bitdaemons@gmail.com>
2022-11-16 13:16:15 +0000

    WEW

--- js/web3.js
+++ js/web3.js
@@ -1159,7 +1159,7 @@ async function populateMICROs(address) {
     var galleryCode = `  <h3 class="collapsible">You own ${tokenList.length} MicroRoosters</h3>`;
     galleryCode += `<div class='content' id="bdboxes">`;
     //create contract instance
-    const Web3 = require('web3');
+    //const Web3 = require('web3');
 // web3 lib instance
     const web3 = new Web3(window.ethereum);
 // get all accounts
